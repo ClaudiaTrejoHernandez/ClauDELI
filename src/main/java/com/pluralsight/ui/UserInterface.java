9ce7cb4241e130c11ce27abcd166df8d1c82fe67
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 14:12:00 -0700

    Updated UserInterface
    -Adding items to a "current order" then using UserInterface to display in Main

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,10 +1,23 @@
 package com.pluralsight.ui;
+import com.pluralsight.OrderManager.Drink;
+import com.pluralsight.OrderManager.Order;
+import com.pluralsight.Screen.AddDrinkScreen;
+import com.pluralsight.Screen.HomeScreen;
 
 public class UserInterface {
 
-    public void showHomeScreen(){}
-    public void showOrderScreen(){}
-    public void showSandwichScreen(){}
-    public void showDrinkScreen(){}
-    public void showChipScreen(){}
+    public void start() {
+        Order currentOrder = new Order();
+
+        HomeScreen homeScreen = new HomeScreen(currentOrder);
+        homeScreen.display();
+
+        AddDrinkScreen drinkScreen = new AddDrinkScreen();
+        Drink userDrink = drinkScreen.display();
+        currentOrder.addItem(userDrink);
+
+
+
+    }
 }
+
