57e7d5d2a37061d857f87714c26e1c56943b2421
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 14:23:08 -0700

    Update to implement PricedItem and return name and price

--- src/main/java/com/pluralsight/OrderManager/Sandwich.java
+++ src/main/java/com/pluralsight/OrderManager/Sandwich.java
@@ -11,56 +11,25 @@ public class Sandwich implements PricedItem{
     protected String size;
     boolean isToasted;
 
-    protected List<Topping> meats;
-    protected List<Topping> cheeses;
-    protected List<Topping> sauces;
-    protected List<Topping> regularToppings;
-
-    public Sandwich(String bread, String size, boolean isToasted, List<Topping> meats, List<Topping> cheeses, List<Topping> sauces, List<Topping> regularToppings) {
+    public Sandwich(String bread, String size, boolean isToasted) {
         this.bread = bread;
         this.size = size;
         this.isToasted = isToasted;
-        this.meats = meats;
-        this.cheeses = cheeses;
-        this.sauces = sauces;
-        this.regularToppings = regularToppings;
     }
 
-    public String getBread() {
-        return bread;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public boolean isToasted() {
-        return isToasted;
-    }
-
-    public List<Topping> getMeats() {
-        return meats;
-    }
-
-    public List<Topping> getCheeses() {
-        return cheeses;
-    }
-
-    public List<Topping> getSauces() {
-        return sauces;
-    }
-
-    public List<Topping> getRegularToppings() {
-        return regularToppings;
-    }
 
     @Override
     public String getName() {
-        return "";
+        return size + "\"" + (isToasted ? "Toasted" : "") + bread + "Sandwich";
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        return switch (size){
+            case "4" -> 5.50;
+            case "8" -> 7.00;
+            case "12" -> 8.50;
+            default -> 0.0;
+        };
     }
 }
