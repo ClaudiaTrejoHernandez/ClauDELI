e6b2b79e977aff62576e74eb1f1cadef46c6e640
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:48:39 -0700

    Implemented to Topping

--- src/main/java/com/pluralsight/Extra/PremiumTopping.java
+++ src/main/java/com/pluralsight/Extra/PremiumTopping.java
@@ -1,4 +1,34 @@
 package com.pluralsight.Extra;
 
-public class PremiumTopping {
+import com.pluralsight.Interface.Topping;
+
+public class PremiumTopping implements Topping{
+
+    private String name;
+    private boolean basePrice;
+    private boolean extra;
+
+    public PremiumTopping(String name, boolean basePrice, boolean extra) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.extra = extra;
+    }
+
+    public String getName() {
+        return name + (extra ? " (extra" : "");
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+//    @Override
+//    public double getPrice() {
+//        return null;
+//    }
+
+    public boolean isExtra() {
+        return extra;
+    }
 }
