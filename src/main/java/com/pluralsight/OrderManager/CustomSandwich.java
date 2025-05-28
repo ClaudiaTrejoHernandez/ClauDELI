b44c28f1c363744251668766f4072c09ed9287f4
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:58:19 -0700

    Update/setup CustomSandwich

--- src/main/java/com/pluralsight/OrderManager/CustomSandwich.java
+++ src/main/java/com/pluralsight/OrderManager/CustomSandwich.java
@@ -1,4 +1,38 @@
 package com.pluralsight.OrderManager;
 
-public class CustomSandwich {
+import com.pluralsight.Interface.Topping;
+import java.util.List;
+
+public class CustomSandwich extends Sandwich {
+
+    public CustomSandwich(String bread, String size, boolean isToasted,
+                          List<Topping> meats,
+                          List<Topping> cheeses,
+                          List<Topping> sauces,
+                          List<Topping> regularToppings) {
+
+        super(bread, size, isToasted, meats, cheeses, sauces, regularToppings);
+    }
+
+    @Override
+    public String getBread() {
+        return null;
+    }
+    @Override
+    public double getPrice() {
+        double basePrice = switch (size) {
+            case "4" -> 5.5;
+            case "8" -> 7.0;
+            case "12" -> 8.5;
+            default -> 0.0;
+        };
+
+        double meatCost = meats.size() * 0.75;
+        double cheeseCost = cheeses.size() * .50;
+        double toppingCost = regularToppings.size() * 0.30;
+
+        return basePrice + meatCost + cheeseCost + toppingCost;
+
+
+    }
 }
