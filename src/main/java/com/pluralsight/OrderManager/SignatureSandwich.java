3c3bf5d944ef5ae3f9d473c19cb4b09c327c081c
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:59:35 -0700

    Update/setup SignatureSandwich
    -initial setup
    -may or may not delete later

--- src/main/java/com/pluralsight/OrderManager/SignatureSandwich.java
+++ src/main/java/com/pluralsight/OrderManager/SignatureSandwich.java
@@ -1,4 +1,13 @@
 package com.pluralsight.OrderManager;
 
-public class SignatureSandwich {
+import com.pluralsight.Interface.Topping;
+
+import java.util.List;
+
+public class SignatureSandwich extends Sandwich{
+
+
+    public SignatureSandwich(String bread, String size, boolean isToasted, List<Topping> meats, List<Topping> cheeses, List<Topping> sauces, List<Topping> regularToppings) {
+        super(bread, size, isToasted, meats, cheeses, sauces, regularToppings);
+    }
 }
