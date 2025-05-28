2e0ddfd684bdac8632f560156e731f6b5600ac63
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:54:37 -0700

    Update/initial setup for Receipt

--- src/main/java/com/pluralsight/Interface/Receipt.java
+++ src/main/java/com/pluralsight/Interface/Receipt.java
@@ -1,4 +1,9 @@
 package com.pluralsight.Interface;
 
+import com.pluralsight.OrderManager.Order;
+
 public interface Receipt {
+
+    String orderDetails(Order order);
+     void createReceiptFile(Order order);
 }
