2c6231812b096550f57aab88580703c627ae3365
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:49:02 -0700

    Implemented to Topping

--- src/main/java/com/pluralsight/Extra/RegularTopping.java
+++ src/main/java/com/pluralsight/Extra/RegularTopping.java
@@ -1,4 +1,29 @@
 package com.pluralsight.Extra;
 
-public class RegularTopping {
+import com.pluralsight.Interface.Topping;
+
+public class RegularTopping implements Topping {
+
+    private String name;
+    private boolean extra;
+
+    public RegularTopping(String name, boolean extra) {
+        this.name = name;
+        this.extra = extra;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public boolean isExtra() {
+        return extra;
+    }
 }
