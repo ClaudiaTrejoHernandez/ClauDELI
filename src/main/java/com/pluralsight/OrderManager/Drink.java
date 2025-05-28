2e8f25f57671f8d862354eaba6a70b7e971b4474
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:57:12 -0700

    Update Drink
    -Implemented methods from PricedItem

--- src/main/java/com/pluralsight/OrderManager/Drink.java
+++ src/main/java/com/pluralsight/OrderManager/Drink.java
@@ -1,4 +1,29 @@
 package com.pluralsight.OrderManager;
 
-public class Drink {
+import com.pluralsight.Interface.PricedItem;
+
+public class Drink implements PricedItem {
+
+    private String size;
+    private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public String getName() {
+        return size + " " + flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return switch (size) {
+            case "Small" -> 1.50;
+            case "Medium" -> 2.00;
+            case "Large" -> 2.50;
+            default -> 0.0;
+        };
+    }
 }
