f29ba7db3dd574b55304a5d724cb2e2d7a1fac5e
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-28 13:55:33 -0700

    Update/setup Main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         UserInterface userInterface = new UserInterface();
-        userInterface.showHomeScreen();
-        
+        userInterface.start();
+
     }
 }
