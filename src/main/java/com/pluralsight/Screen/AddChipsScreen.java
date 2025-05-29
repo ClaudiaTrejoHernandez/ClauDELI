0a3fcf676db8c5871839b7ce355592229c51144d
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-29 12:47:57 -0700

    Update Screen display
    -I'm adding separators and making it more colorful and appealing to look at
    -also renamed some chip options

--- src/main/java/com/pluralsight/Screen/AddChipsScreen.java
+++ src/main/java/com/pluralsight/Screen/AddChipsScreen.java
@@ -6,15 +6,23 @@ import com.pluralsight.Utility.ConsoleHelper;
 
 public class AddChipsScreen implements Screen<Chip> {
 
+    @Override
     public Chip display() {
+        System.out.println("✦🍃━━━━━━━━━━━━━༺☁︎｡⋆｡ ﾟ☾ ﾟ｡⋆｡☁︎༻━━━━━━━━━━━━━━━🍃✦");
+        System.out.println("         🍟 Choose Your Chips 🍟");
+        System.out.println("✦🍃━━━━━━━━━━━━━༺｡⋆｡☾｡⋆｡☁︎｡⋆｡☁︎༻━━━━━━━━━━━━━━━🍃✦\n");
+
         int chipChoice = ConsoleHelper.readInt(
-                "Choose your chips:\n1) BBQ\n2) Sour Cream & Onion\n3) Salt & Vinegar\n4) Classic", 1,4);
+                "🥔 1) Lay's BBQ\n" +
+                        "🥔 2) Sour Cream & Onion\n" +
+                        "🥔 3) Salt & Vinegar\n" +
+                        "🥔 4) Fritos", 1, 4);
 
         String flavor = switch (chipChoice) {
-            case 1 -> "BBQ";
+            case 1 -> "Lay's BBQ";
             case 2 -> "Sour Cream & Onion";
             case 3 -> "Salt & Vinegar";
-            case 4 -> "Classic";
+            case 4 -> "Fritos";
             default -> throw new IllegalStateException("Unexpected value: " + chipChoice);
         };
 
