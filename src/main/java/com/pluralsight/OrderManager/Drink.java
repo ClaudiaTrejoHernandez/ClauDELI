210e3d5b5aa1568f47c2716b2cc18a62e5e12a14
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-29 00:57:25 -0700

    Update Drink
    -Implementing PricedItem
    --to getPrice
    --and getName

--- src/main/java/com/pluralsight/OrderManager/Drink.java
+++ src/main/java/com/pluralsight/OrderManager/Drink.java
@@ -6,10 +6,21 @@ public class Drink implements PricedItem {
 
     private String size;
     private String flavor;
+    private double price;
 
     public Drink(String size, String flavor) {
         this.size = size;
         this.flavor = flavor;
+        this.price = getPriceBasedOnSize(size);
+    }
+
+    private double getPriceBasedOnSize(String size) {
+        return switch (size) {
+            case "Small" -> 2.00;
+            case "Medium" -> 2.50;
+            case "Large" -> 3.00;
+            default -> 0.0;
+        };
     }
 
     @Override
@@ -19,11 +30,6 @@ public class Drink implements PricedItem {
 
     @Override
     public double getPrice() {
-        return switch (size) {
-            case "Small" -> 1.50;
-            case "Medium" -> 2.00;
-            case "Large" -> 2.50;
-            default -> 0.0;
-        };
+        return price;
     }
 }
