202a93479bdedbf740fa42dc5b0a0ff11dda3c8c
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-29 00:54:54 -0700

    Update Chip
    -Implementing PricedItem to make chip getName & getPrice

--- src/main/java/com/pluralsight/OrderManager/Chip.java
+++ src/main/java/com/pluralsight/OrderManager/Chip.java
@@ -3,16 +3,20 @@ package com.pluralsight.OrderManager;
 import com.pluralsight.Interface.PricedItem;
 
 public class Chip implements PricedItem {
-
     private String flavor;
+    private double price = 1.00;
+
+    public Chip(String flavor) {
+        this.flavor = flavor;
+    }
 
     @Override
     public String getName() {
-        return "";
+        return flavor + " Chips";
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        return price;
     }
 }
