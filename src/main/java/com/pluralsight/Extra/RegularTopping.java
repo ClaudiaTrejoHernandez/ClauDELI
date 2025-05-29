399f44e51b9d1ade8caf453d9214e1a819c49cbc
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-29 00:50:02 -0700

    Update Regular toppings (meat and cheese)
    -Since these are regular and only are extra cost if extra are requested, they are here and implement Toppings

--- src/main/java/com/pluralsight/Extra/RegularTopping.java
+++ src/main/java/com/pluralsight/Extra/RegularTopping.java
@@ -5,25 +5,82 @@ import com.pluralsight.Interface.Topping;
 public class RegularTopping implements Topping {
 
     private String name;
-    private boolean extra;
+    private String type;
+    private boolean isExtra;
+    private String size;
 
-    public RegularTopping(String name, boolean extra) {
+    //Constructor for no extra toppings
+    public RegularTopping(String name, String type, boolean isExtra, String size) {
         this.name = name;
-        this.extra = extra;
+        this.type = type;
+        this.isExtra = isExtra;
+        this.size = size;
     }
 
     @Override
     public String getName() {
-        return "";
+        return (isExtra ? "Extra " : "") + name;
     }
 
     @Override
-    public double getPrice() {
-        return 0;
+    public double getTotalPrice() {
+        return getPrice(size);
     }
 
     @Override
+    public String getType() {
+        return type;
+    }
+
+
+
+    public double getPrice(String size) {
+        return switch (type) {
+            case "meat" -> isExtra ? getExtraMeatPrice(this.size) : getMeatPrice(this.size);
+            case "cheese" -> isExtra ? getExtraCheesePrice(this.size) : getCheesePrice(this.size);
+            default -> 0.0;
+        };
+    }
+
+    public double getMeatPrice(String size) {
+        return switch (size) {
+            case "4" -> 1.00;
+            case "8" -> 2.00;
+            case "12" -> 3.00;
+            default -> 0.0;
+        };
+    }
+
+    private double getExtraMeatPrice(String size) {
+        return switch (size) {
+            case "4" -> 0.50;
+            case "8" -> 1.00;
+            case "12" -> 1.50;
+            default -> 0.0;
+        };
+    }
+
+    private double getCheesePrice(String size) {
+        return switch (size) {
+            case "4" -> 0.75;
+            case "8" -> 1.50;
+            case "12" -> 2.25;
+            default -> 0.0;
+        };
+    }
+
+    private double getExtraCheesePrice(String size) {
+        return switch (size) {
+            case "4" -> 0.30;
+            case "8" -> 0.60;
+            case "12" -> 0.90;
+            default -> 0.0;
+        };
+    }
+
     public boolean isExtra() {
-        return extra;
+        return isExtra;
     }
+
+
 }
